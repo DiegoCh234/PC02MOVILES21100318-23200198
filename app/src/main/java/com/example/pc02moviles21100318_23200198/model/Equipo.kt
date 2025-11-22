554719e2a754572bc6c2a20f9226022bae66fda4
mollinedo-cmdo <brayanmegatrom1234@gmail.com>
2025-11-22 10:25:24 -0500

    subiendo firabase

---
+++ app/src/main/java/com/example/pc02moviles21100318_23200198/model/Equipo.kt
@@ -0,0 +1,8 @@
+package com.example.pc02moviles21100318_23200198.model
+
+data class Equipo(
+    val nombre: String = "",
+    val anioFundacion: String = "",
+    val titulos: String = "",
+    val urlImagen: String = ""
+)
